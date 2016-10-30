5f8969be1104d3bc63e80b9b7133b435c6f679d8
fuxicoder <fuxicoder@gmail.com>
2016-10-30 22:19:10 +0800

    Create xml.py
    test

---
+++ xml.py
@@ -0,0 +1,52 @@
+from xml.dom import minidom 
+import traceback
+
+def fun1():
+    f = open("xmlstuff.xml", "w") 
+     
+    doc = minidom.Document() 
+    rootNode = doc.createElement("root") 
+    doc.appendChild(rootNode) 
+    bookNode = doc.createElement("book") 
+    bookNode.setAttribute("isbn", "34909023") 
+    rootNode.appendChild(bookNode) 
+    authorNode = doc.createElement("author") 
+    bookNode.appendChild(authorNode) 
+    authorTextNode = doc.createTextNode("dikatour") 
+    authorNode.appendChild(authorTextNode) 
+    doc.writexml(f, "\t", "\t", "\n", "utf-8") 
+    f.close() 
+def fun2():
+    f2 = open("xmlstuff.xml", "r")
+    strl= ""
+    for line in f2.readlines():
+        strl = strl + str(line).strip()
+    f2.close()
+    f2 = open("xmlstuff.xml", "w")
+    f2.write(strl)
+    f2.close()
+    doc = minidom.parse("xmlstuff.xml")
+    rootNode = doc.getElementsByTagName('root')[0]
+    tmpDoc = minidom.Document().createElement('book')
+    tmpDoc.setAttribute("isbn", "34909023")  
+    authorNode = doc.createElement("author")
+    authorTextNode = doc.createTextNode("dikatour")
+    authorNode.appendChild(authorTextNode)
+    tmpDoc.appendChild(authorNode)
+    #rootNode.appendChild(tmpDoc)
+    for node in rootNode.getElementsByTagName('book'):
+        for node1 in node.getElementsByTagName('author'):
+            if node1.hasChildNodes():
+                node2 = node1.firstChild
+                print node2.nodeType
+                print node2.nodeType == node1.TEXT_NODE
+                print node2.nodeValue
+
+        #print node.isSameNode(tmpDoc)
+    
+    f = open("xmlstuff.xml", "w")
+    doc.writexml(f, "\t", "\t", "\n", "utf-8") 
+    f.close()     
+if __name__=="__main__":
+    fun2()
+    pass
